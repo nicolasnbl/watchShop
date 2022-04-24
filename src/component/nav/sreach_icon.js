6befd3d00e05ee6a1feb3cb5837627c8b8d6ff6a
nicolasnbl <noblet.nicolas07@gamil.com>
2022-04-24 17:37:11 +0200

    Pour les vacances

--- src/component/nav/sreach_icon.js
+++ src/component/nav/sreach_icon.js
@@ -3,13 +3,14 @@ import React, {useEffect, useState} from 'react';
 const Search = () => {
 
   var classNameTxt = "txt-search";
+  var classNameTrait = "trait-Search";
 
   const [isVisible, setVisible] = useState(false);
 
-  classNameTxt = isVisible ? "txt-search" : "txt-search display-none"
+  classNameTxt = isVisible ? "txt-search text-show" : "txt-search display-none text-hide";
+  classNameTrait = isVisible ? "trait-Search TraitSearchShow" : "trait-Search";
 
   const changeVisibility = () =>{
-    console.log('yo');
     if(isVisible){
       setVisible(false); 
     }else {
@@ -20,15 +21,13 @@ const Search = () => {
 
   return (
     <div className="block-search" >
-        <div className={classNameTxt}>
-          Que recherchez-vous ?
-        </div>
+        <input type="text" className={classNameTxt} placeholder="Que recherchez-vous ?" />
         <span className="icon"  onClick={() => changeVisibility()}>
           <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" id="search" type='button'>
               <path d="M17.17 16.48L12 11.36a5.5 5.5 0 10-4.22 2 5.41 5.41 0 003.51-1.27l5.14 5.13a.51.51 0 00.7 0 .5.5 0 00.04-.74zm-9.35-4.15a4.5 4.5 0 110-9 4.5 4.5 0 010 9z" ></path>
           </svg>
         </span>
-        <div className="trait-Search"></div>
+        <div className={classNameTrait}></div>
     </div>
   )
 }
